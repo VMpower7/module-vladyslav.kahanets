b8e32002f776dd75c3da092aba9510cfca19a6bd
VMpower7 <115544666+VMpower7@users.noreply.github.com>
2023-12-10 21:24:44 +0200

    Create for_modul.sql

---
+++ for_modul.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    age INTEGER,
+    gender TEXT,
+    email TEXT UNIQUE,
+    verification_code INTEGER
+);
